785c7b736c1fa5d6fded86de2d212765431979d6
Angelo Zamora <angelo.zamora@unmsm.edu.pe>
2022-06-07 00:19:21 -0500

    [updted] config axios

--- src/jwt/jwtDefaultConfig.ts
+++ src/jwt/jwtDefaultConfig.ts
@@ -1,3 +1,8 @@
+import PokemonApi from "./api/pokemonApi";
+
 export default {
-  
+  tokenType: "Bearer",
+  storageTokenKeyName: "accessTokenPA",
+  storageRefreshTokenKeyName: "refreshTokenPA",
+  ...PokemonApi
 }
---
+++ src/jwt/useJwt.ts
@@ -0,0 +1,13 @@
+import axios, { AxiosInstance } from "axios";
+import JwtService from "./jwtService";
+
+function useJwt(axiosIns: AxiosInstance, jwtOverrideConfig:any){
+  const jwt = new JwtService(axiosIns, jwtOverrideConfig)
+  return {jwt}
+}
+
+const {jwt} = useJwt(axios, {})
+console.log('PRUEBAAAAAAAAAAAAA');
+console.log(jwt);
+console.log('PRUEBAAAAAAAAAAAAA');
+export default jwt
